726df9c3640cae93c06b5fd70589505186dec021
Minh Nam <duongminhnam99@gmail.com>
2022-02-27 17:15:20 +0700

    [Update] add event simplewallet

--- SimpleWallet/SimpleWallet.sol
+++ SimpleWallet/SimpleWallet.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract Allowance is Ownable{
+contract Allowance is Ownable {
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
+
     mapping(address => uint) public allowance;
 
     function addAllowance(address _who, uint _amount) public {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
 
@@ -20,6 +23,7 @@ contract Allowance is Ownable{
     }
 
     function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
         allowance[_who] -= _amount;
     }
 
