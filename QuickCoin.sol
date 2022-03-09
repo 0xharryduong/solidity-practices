f47f57facbc2724678544ccc3248d2ab19bf9d0a
Minh Nam <duongminhnam99@gmail.com>
2022-03-09 20:50:54 +0700

    [Update] Quickcoin basic

--- QuickCoin.sol
+++ QuickCoin.sol
@@ -6,18 +6,32 @@ contract QuickCoin {
 
     event sent(address from, address to, uint amount);
 
+    modifier onlyMinter {
+        require(msg.sender == minter);
+        _;
+    }
+
+    modifier checkAmount(uint amount) {
+        require(amount < 1e60);
+        _;
+    }
+
+    modifier checkBalance(uint amount) {
+        require(amount <= balances[msg.sender], "Insufficient funds");
+        _;
+    }
+
     constructor() {
         minter = msg.sender;
     }
 
-    function mint(address receiver, uint amount) public {
+    function mint(address receiver, uint amount) public onlyMinter checkAmount(amount) {
         require(msg.sender == minter);
         require(amount < 1e60);
         balances[receiver] += amount;
     }
 
-    function send(address receiver, uint amount) public {
-        require(amount <= balances[msg.sender], "Insufficient funds");
+    function send(address receiver, uint amount) public checkBalance(amount) {
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
         emit sent(msg.sender, receiver, amount);
